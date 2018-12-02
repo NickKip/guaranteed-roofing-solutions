d8c4a7ebe389d71ba402fd83966d3f9556e194a7
Nick Kipling <nick.kipling@commontime.com>
2018-12-02 20:32:02 +0000

    Latest updates

--- src/assets/scripts/image-rotation.js
+++ src/assets/scripts/image-rotation.js
@@ -4,16 +4,23 @@ window.rotate = function () {
     var pos = 1;
 
     var images = [
-
-        "/img/rotation/IMG_1346.jpg",
-        "/img/rotation/IMG_1348.jpg",
-        "/img/rotation/IMG_1358.jpg",
-        "/img/rotation/IMG_1372.jpg",
-        "/img/rotation/IMG_1373.jpg",
-        "/img/rotation/IMG_1398.jpg",
-        "/img/rotation/IMG_1401.jpg",
-        "/img/rotation/IMG_1402.jpg",
-        "/img/rotation/IMG_1452.jpg"
+        "/img/rotation/IMG_1452.jpg",
+        "/img/rotation/IMG_1682.jpg",
+        "/img/rotation/IMG_1709.jpg",
+        "/img/rotation/IMG_1863.jpg",
+        "/img/rotation/IMG_1876.jpg",
+        "/img/rotation/IMG_1878.jpg",
+        "/img/rotation/IMG_1880.jpg",
+        "/img/rotation/IMG_1936.jpg",
+        "/img/rotation/IMG_1949.jpg",
+        "/img/rotation/IMG_2030.jpg",
+        "/img/rotation/IMG_2032.jpg",
+        "/img/rotation/IMG_2056.jpg",
+        "/img/rotation/IMG_2069.jpg",
+        "/img/rotation/IMG_2078.jpg",
+        "/img/rotation/IMG_2108.jpg",
+        "/img/rotation/IMG_2115.jpg",
+        "/img/rotation/IMG_2118.jpg"
     ];
 
     if (container) {
---
+++ src/html/accreditations.md
@@ -0,0 +1,23 @@
+---
+title: Guaranteed Roofing Solutions Accreditations
+heading: Accreditations, Certificates and Policies
+description: Test
+layout: basic.html
+priority: 0.9
+---
+
+<a href="/pdfs/CHAS-Certificate.pdf" target="_blank">CHAS Certificate of Accreditation</a>
+
+<a href="/pdfs/SMSTS.pdf" target="_blank">Site Safety Plus</a>
+
+<a href="/pdfs/Asbestos-Awareness.pdf" target="_blank">Asbestos Awareness</a>
+
+<a href="/pdfs/Safety-First-Package.jpeg" target="_blank">Safety First Package</a>
+
+<a href="/pdfs/Environmental-Policy Statement-converted.pdf" target="_blank">Environmental Policy</a>
+
+<a href="/pdfs/Health-and-Saftey-Policy.pdf" target="_blank">Health &amp; Safety Policy</a>
+
+<a href="/pdfs/Public-Liabilty-Insurance .pdf" target="_blank">Public Liability Insurance</a>
+
+<a href="/pdfs/EL-Certificate-(KIBX62NK01).pdf" target="_blank">Employers Liability Insurance</a>
